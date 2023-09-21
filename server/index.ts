e48125c174aa0fee658f43ad123339ee10f62a70
dnozaki <dnozaki88888888@gmail.com>
2023-09-21 15:02:13 +0900

    make userSchema u013

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,15 @@
-const express = require('express');
+// const express = require('express');
+import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Express!');
+});
+
+//ユーザー新規登録API
+
+//ユーザーログイン用API
 
 app.listen(PORT, () => {
   console.log('サーバー起動中・・・');
