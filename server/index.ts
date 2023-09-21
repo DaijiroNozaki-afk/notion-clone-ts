9306c717cb61b53a0945cace348e1ca8270aa442
dnozaki <dnozaki88888888@gmail.com>
2023-09-21 15:13:07 +0900

    set dotenv u014

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 // const express = require('express');
 import express, { Request, Response } from 'express';
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
+require('dotenv').config();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Express!');
-});
+//DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log('DB と接続中・・・');
+} catch (err) {
+  console.log(err);
+}
 
 //ユーザー新規登録API
 
