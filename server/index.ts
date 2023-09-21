873214e933fc7c3fb4683adfeaa4d68ca25f5a2d
dnozaki <dnozaki88888888@gmail.com>
2023-09-21 16:20:21 +0900

    make encrypt password u017

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 // const express = require('express');
 import express, { Request, Response } from 'express';
 const mongoose = require('mongoose');
+const CryptoJS = require('crypto-js');
+const User = require('./src/v1/models/user');
 const app = express();
 const PORT = 5000;
 require('dotenv').config();
@@ -14,6 +16,19 @@ try {
 }
 
 //ユーザー新規登録API
+
+app.post('/register', async (req, res) => {
+  //パスワードの受け取り
+  const password = req.body.password;
+  try {
+    //パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    //ユーザーの新規作成
+    const user = await User.create(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //ユーザーログイン用API
 
