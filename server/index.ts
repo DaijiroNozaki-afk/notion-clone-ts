bbff4e2c7a54f04d46c18652e759f27087d16c44
dnozaki <dnozaki88888888@gmail.com>
2023-09-22 11:54:23 +0900

    use validation u022

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 const mongoose = require('mongoose');
 const CryptoJS = require('crypto-js');
 const JWT = require('jsonwebtoken');
+const { body } = require('express-validator');
 const User = require('./src/v1/models/user');
 const app = express();
 const PORT = 5000;
@@ -27,6 +28,15 @@ try {
 //ユーザー新規登録API
 app.post(
   '/register',
+  body('username')
+    .isLength({ min: 8 })
+    .withMessage('ユーザー名は8文字以上ある必要があります。'),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('パスワードは8文字以上ある必要があります。'),
+  body('confirmPassword')
+    .isLength({ min: 8 })
+    .withMessage('確認用パスワードは8文字以上ある必要があります。'),
   async (req: Request<{}, {}, RegisterRequest>, res: Response) => {
     //パスワードの受け取り
     const password = req.body.password;
