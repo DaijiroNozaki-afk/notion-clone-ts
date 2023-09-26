424644aab18ccc97379a51a4099a3dd7c0368e91
dnozaki <dnozaki88888888@gmail.com>
2023-09-26 11:33:17 +0900

    make registerComponent u038

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
-  return <div className="App">test</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
