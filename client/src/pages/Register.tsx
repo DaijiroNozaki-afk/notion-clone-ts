29bfc11e54b64fd9759703f6551fdeed021f1157
dnozaki <dnozaki88888888@gmail.com>
2023-09-27 13:29:38 +0900

    check register u050

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -3,20 +3,32 @@ import { Box, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { LoadingButton } from '@mui/lab';
 import { Link } from 'react-router-dom';
+import authApi from '../api/authApi';
 
 const Register = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     //入力欄の文字列を取得
     const data = new FormData(e.target as HTMLFormElement);
-    const username = (data.get('username') as string | null)?.trim();
-    const password = (data.get('password') as string | null)?.trim();
-    const confirmPassword = (
-      data.get('confirmPassword') as string | null
-    )?.trim();
-    console.log(username);
-    console.log(password);
-    console.log(confirmPassword);
+    const username = (data.get('username') || '').toString().trim();
+    const password = (data.get('password') || '').toString().trim();
+    const confirmPassword = (data.get('confirmPassword') || '')
+      .toString()
+      .trim();
+
+    // 新規登録API を叩く
+    try {
+      const res = await authApi.register({
+        username,
+        password,
+        confirmPassword,
+      });
+      const token = res.data.token;
+      localStorage.setItem('token', token);
+      console.log('新規登録に成功しました。');
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <>
