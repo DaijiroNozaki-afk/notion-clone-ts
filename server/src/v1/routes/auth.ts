58c9470d6b4e7217bc3c6f285ba1b1dced7ccf88
dnozaki <dnozaki88888888@gmail.com>
2023-10-13 15:03:37 +0900

    make homeComponent u079

--- server/src/v1/routes/auth.ts
+++ server/src/v1/routes/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { RegisterRequest, UserRequest } from '../types/types';
-const router = require('express').Router();
+import express from 'express';
+const router = express.Router();
 const { body } = require('express-validator');
 require('dotenv').config();
 
--- server/src/v1/routes/memo.ts
+++ server/src/v1/routes/memo.ts
@@ -1,4 +1,8 @@
-const router = require('express').Router();
+import express from 'express';
+const router = express.Router();
+// TypeScriptは通常ESモジュールシステムを使用します。
+// このため、この行によってrouterが再定義され、エラーが発生しています。
+// const router = require('express').Router();
 const memoController = require('../controllers/memo');
 const tokenHandler = require('../handlers/tokenHandler');
 
