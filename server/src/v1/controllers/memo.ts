ebe3eecaf2358ca990517c72e63415b617932055
dnozaki <dnozaki88888888@gmail.com>
2023-10-16 15:04:30 +0900

    getAllMemos u082

--- server/src/v1/controllers/memo.ts
+++ server/src/v1/controllers/memo.ts
@@ -6,6 +6,8 @@ exports.create = async (req: Request<{}, {}, MemoRequest>, res: Response) => {
   try {
     const memoCount = await Memo.find().count();
     // メモ新規作成
+    // console.log(req.user);
+    //req.user でミドルウェアの値は取得できている
     if (req.user !== undefined) {
       const memo = await Memo.create({
         user: req.user._id,
@@ -14,6 +16,18 @@ exports.create = async (req: Request<{}, {}, MemoRequest>, res: Response) => {
       res.status(201).json(memo);
     }
     res.status(204); //json で状況を知らせるメッセージを返す必要がある
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+exports.getAll = async (req: Request<{}, {}, MemoRequest>, res: Response) => {
+  try {
+    if (req.user !== undefined) {
+      const memos = await Memo.find({ user: req.user._id }).sort('-position');
+      res.status(200).json(memos);
+    } else {
+      res.status(404).json('メモが見つかりません。');
+    }
   } catch (err) {
     res.status(500).json(err);
   }
