87510f7c1912cb77a0310821545e2fc420dc890e
dnozaki <dnozaki88888888@gmail.com>
2023-10-18 15:33:30 +0900

    make memoComponent u088

--- client/src/pages/Memo.tsx
+++ client/src/pages/Memo.tsx
@@ -1,7 +1,31 @@
+import { DeleteOutlineOutlined, StarBorderOutlined } from '@mui/icons-material';
+import { Box, Button, IconButton, TextField } from '@mui/material';
 import React from 'react';
 
 const Memo = () => {
-  return <div>Memo</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+        }}
+      >
+        <IconButton>
+          <StarBorderOutlined />
+        </IconButton>
+        <IconButton color="error">
+          <DeleteOutlineOutlined />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: '10px 50px' }}>
+        <TextField placeholder="無題" variant="outlined" fullWidth />
+        <TextField placeholder="追加" variant="outlined" fullWidth />
+      </Box>
+    </>
+  );
 };
 
 export default Memo;
