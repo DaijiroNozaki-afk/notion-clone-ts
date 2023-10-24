e9b888ce3ad5c875e5d96bfb6f1cdabe9249bf33
dnozaki <dnozaki88888888@gmail.com>
2023-10-24 16:03:44 +0900

    fix emojiIconSize u099

--- client/src/components/layout/common/EmojiPicker.tsx
+++ client/src/components/layout/common/EmojiPicker.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 interface emojiInt {
   icon: string;
 }
 
 const EmojiPicker = (props: emojiInt) => {
-  return <div>{props.icon}</div>;
+  const [selectedEmoji, setSelectedEmoji] = useState<string>();
+  useEffect(() => {
+    setSelectedEmoji(props.icon);
+  }, [props.icon]);
+  return (
+    <Box>
+      <Typography variant="h3" fontWeight="700" sx={{ cursor: 'pointer' }}>
+        {selectedEmoji}
+      </Typography>
+    </Box>
+  );
 };
 
 export default EmojiPicker;
