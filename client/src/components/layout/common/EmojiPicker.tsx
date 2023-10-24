ce3d0aefcfdf50c0c402ff8b02c87ad20d3ef5f1
dnozaki <dnozaki88888888@gmail.com>
2023-10-24 16:32:20 +0900

    set emojiPickerdoor u101

--- client/src/components/layout/common/EmojiPicker.tsx
+++ client/src/components/layout/common/EmojiPicker.tsx
@@ -8,15 +8,29 @@ interface emojiInt {
 
 const EmojiPicker = (props: emojiInt) => {
   const [selectedEmoji, setSelectedEmoji] = useState<string>();
+  const [isShowPicker, setIsShowPicker] = useState<boolean>(false);
   useEffect(() => {
     setSelectedEmoji(props.icon);
   }, [props.icon]);
+
+  const showPicker = () => setIsShowPicker(!isShowPicker);
   return (
     <Box>
-      <Typography variant="h3" fontWeight="700" sx={{ cursor: 'pointer' }}>
+      <Typography
+        variant="h3"
+        fontWeight="700"
+        sx={{ cursor: 'pointer' }}
+        onClick={showPicker}
+      >
         {selectedEmoji}
       </Typography>
-      <Box sx={{ display: 'none' }}>
+      <Box
+        sx={{
+          display: isShowPicker ? 'block' : 'none',
+          position: 'absolute',
+          zIndex: '100',
+        }}
+      >
         <Picker />
       </Box>
     </Box>
